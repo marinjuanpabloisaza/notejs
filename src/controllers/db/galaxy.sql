2ecc401d80a3be34830a56eda3db00c3894d4fa1
juan <marinjuanpabloisaza@gmail.com>
2022-03-06 18:34:25 -0500

    proyect

--- src/controllers/db/galaxy.sql
+++ src/controllers/db/galaxy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-02-2022 a las 23:18:08
+-- Tiempo de generación: 01-03-2022 a las 23:03:14
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `galaxy`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `title` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `categories`
+--
+
+INSERT INTO `categories` (`id`, `title`) VALUES
+(1, 'licores'),
+(2, 'Bebidas'),
+(3, 'Cigarrería'),
+(4, 'snacks');
 
 -- --------------------------------------------------------
 
@@ -45,9 +66,39 @@ INSERT INTO `products` (`id`, `title`, `price`, `stock`, `url_image`, `descripti
 (3, 'Coca cola', 8000, 1000, 'https://coca-colafemsa.com/wp-content/uploads/2020/02/2-30.png', '', 2),
 (4, 'cerveza cornona', 8000, 1000, 'https://lacasserole.com.co/wp-content/uploads/2021/03/CERVEZA-CORONA-335-DE-500-X-500.jpg', '', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `telefono` varchar(500) NOT NULL,
+  `role` varchar(30) NOT NULL,
+  `address` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `telefono`, `role`, `address`) VALUES
+(2, 'marin', 'juanmarin@gmail.com', 'juan', '3002995463', 'USER', 'callle sur'),
+(3, 'ad', 'juanmarin@gmail.com', 'juan123', '4546', 'USER', 'gh');
+
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `products`
@@ -57,14 +108,32 @@ ALTER TABLE `products`
   ADD KEY `category_id` (`category_id`);
 
 --
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT de la tabla `products`
 --
 ALTER TABLE `products`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
